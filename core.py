9aa1561e9cf53d580b01489a466add988ccae1e9
Santiago Rodríguez Romero <114857078+SantiagoRR2004@users.noreply.github.com>
2024-12-09 16:10:49 +0100

    Commented code

--- core.py
+++ core.py
@@ -1,9 +1,38 @@
-class CBR:
-    def __init__(self, base_de_casos, num_casos_similares=1):
+from abc import ABC, abstractmethod
+from typing import List, Tuple, Dict
+
+
+class CBR(ABC):
+    def __init__(
+        self, base_de_casos: Dict[int, dict], num_casos_similares: str = 1
+    ) -> None:
+        """
+        Stores the base of cases and the number of similar cases to retrieve.
+
+        Args:
+            - base_de_casos (Dict[int, dict]): The case base. The keys are the case identifiers and
+                the values are the cases themselves.
+            - num_casos_similares (int): The number of similar cases to retrieve.
+
+        Returns:
+            - None
+        """
         self.base_de_casos = base_de_casos
         self.num_casos_similares = num_casos_similares
 
-    def inicializar_caso(self, caso, id=None):
+    def inicializar_caso(self, caso: dict, id: str = None) -> dict:
+        """
+        Initializes a case.
+
+        Initializes a case by adding a _meta attribute to link metadata during the CBR cycle.
+
+        Args:
+            - caso (dict): The case to initialize.
+            - id (str): The case identifier.
+
+        Returns:
+            - dict: The initialized case.
+        """
         # crear un atributo _meta para vincular metadatos durante el ciclo CBR
         caso["_meta"] = {}
 
@@ -18,35 +47,110 @@ class CBR:
 
         return caso
 
-    def recuperar(self, caso_a_resolver):
+    @abstractmethod
+    def recuperar(self, caso_a_resolver: dict) -> Tuple[List[dict], List[float]]:
+        """
+        Retrieves similar cases from the case base.
+
+        This method should be implemented by subclasses.
+
+        Args:
+            - caso_a_resolver (dict): The case to resolve.
+
+        Returns:
+            - tuple: A tuple containing 2 lists:
+                - List[dict]: The similar cases.
+                - List[float]: The similarity scores.
+        """
         pass
 
-    def reutilizar(self, caso_a_resolver, casos_similares, similaridades):
+    @abstractmethod
+    def reutilizar(
+        self,
+        caso_a_resolver: dict,
+        casos_similares: List[dict],
+        similaridades: List[float],
+    ) -> dict:
+        """
+        Reuses similar cases to resolve the current case.
+
+        This method should be implemented by subclasses.
+
+        Args:
+            - caso_a_resolver (dict): The case to resolve.
+            - casos_similares (List[dict]): The similar cases.
+            - similaridades (List[float]): The similarity scores.
+
+        Returns:
+            - dict: The resolved case.
+        """
         pass
 
+    @abstractmethod
     def revisar(
         self,
-        caso_resuelto,
-        caso_a_resolver=None,
-        casos_similares=None,
-        similaridades=None,
-    ):
+        caso_resuelto: dict,
+        caso_a_resolver: dict = None,
+        casos_similares: List[dict] = None,
+        similaridades: List[float] = None,
+    ) -> dict:
+        """
+        Reviews the resolved case.
+
+        This method should be implemented by subclasses.
+
+        Args:
+            - caso_resuelto (dict): The resolved case.
+            - caso_a_resolver (dict): The case to resolve.
+            - casos_similares (List[dict]): The similar cases.
+            - similaridades (List[float]): The similarity scores.
+
+        Returns:
+            - dict: The reviewed case.
+        """
         pass
 
+    @abstractmethod
     def retener(
         self,
-        caso_revisado,
-        caso_a_resolver=None,
-        casos_similares=None,
-        similaridades=None,
-    ):
+        caso_revisado: dict,
+        caso_a_resolver: dict = None,
+        casos_similares: List[dict] = None,
+        similaridades: List[float] = None,
+    ) -> None:
+        """
+        Retains the reviewed case if deemed necessary.
+
+        This method should be implemented by subclasses.
+
+        Args:
+            - caso_revisado (dict): The reviewed case.
+            - caso_a_resolver (dict): The case to resolve.
+            - casos_similares (List[dict]): The similar cases.
+            - similaridades (List[float]): The similarity scores.
+
+        Returns:
+            - None
+        """
         pass
 
-    def ciclo_cbr(self, caso_a_resolver, id_caso=None):
+    def ciclo_cbr(self, caso_a_resolver: dict, id_caso: str = None) -> dict:
+        """
+        Executes a CBR cycle to resolve a case.
+
+        First, initializes the case to resolve. Then, retrieves similar cases from the case base.
+        Next, reuses the similar cases to resolve the current case. After that, reviews the resolved
+        case. Finally, retains the reviewed case if deemed necessary.
+
+        Args:
+            - caso_a_resolver (dict): The case to resolve.
+            - id_caso (str): The case identifier.
+
+        Returns:
+            - dict: The finished case.
+        """
         caso_a_resolver = self.inicializar_caso(caso_a_resolver, id=id_caso)
-        (casos_similares, similaridades) = self.recuperar(
-            caso_a_resolver
-        )  # , base_de_casos, similaridad)
+        (casos_similares, similaridades) = self.recuperar(caso_a_resolver)
         caso_resuelto = self.reutilizar(caso_a_resolver, casos_similares, similaridades)
         caso_revisado = self.revisar(
             caso_resuelto, caso_a_resolver, casos_similares, similaridades
@@ -56,15 +160,55 @@ class CBR:
 
 
 class CBR_DEBUG:
-    def __init__(self, prettyprint_caso):
+    def __init__(self, prettyprint_caso: callable) -> None:
+        """
+        Stores the prettyprint function. It needs to
+        format a case (dict) into a human-readable string.
+
+        Args:
+            - prettyprint_caso (callable): The prettyprint function.
+
+        Returns:
+            - None
+        """
         self.prettyprint_caso = prettyprint_caso
 
-    def debug_mensaje(self, etiqueta, mensaje=""):
+    def debug_mensaje(self, etiqueta: str, mensaje: str = "") -> None:
+        """
+        Prints a debug message. The etiqueta is shown between brackets
+        before the message.
+
+        Args:
+            - etiqueta (str): The debug message label.
+            - mensaje (str): The debug message.
+
+        Returns:
+            - None
+        """
         print("[{}] {}".format(etiqueta, mensaje))
 
-    def debug_recuperar(self, caso, similares, scores):
+    def debug_recuperar(
+        self, caso: dict, similares: List[dict], scores: List[float]
+    ) -> None:
+        """
+        Prints debug information about the retrieval step.
+
+        First, it prints the case to resolve. Then, it prints the value
+        of the best similarity score and then it prints the similar cases.
+
+        similares and scores need to be the same length.
+
+        Args:
+            - caso (dict): The case to resolve.
+            - similares (List[dict]): The similar cases.
+            - scores (List[float]): The similarity scores.
+
+        Returns:
+            - None
+        """
         self.debug_mensaje(
-            "DEBUG.recuperar", mensaje="CASO A RESOLVER: " + self.prettyprint_caso(caso)
+            "DEBUG.recuperar",
+            mensaje="CASO A RESOLVER:\n" + self.prettyprint_caso(caso),
         )
         self.debug_mensaje(
             "DEBUG.recuperar",
@@ -77,24 +221,46 @@ class CBR_DEBUG:
         for c, score in zip(similares, scores):
             self.debug_mensaje(
                 "DEBUG.recuperar",
-                mensaje="- [{}] {}  (Similaridad: {})".format(
-                    count, self.prettyprint_caso(c), score
+                mensaje="- [{}] (Similaridad: {})\n{}".format(
+                    count, score, self.prettyprint_caso(c)
                 ),
             )
             count = count + 1
         self.debug_mensaje("DEBUG.recuperar")
 
-    def debug_reutilizar(self, caso_resuelto):
+    def debug_reutilizar(self, caso_resuelto: dict) -> None:
+        """
+        Prints the information about the solved case.
+
+        Args:
+            - caso_resuelto (dict): The solved case.
+
+        Returns:
+            - None
+        """
         self.debug_mensaje(
             "DEBUG.reutilizar",
-            mensaje="CASO RESUELTO: " + self.prettyprint_caso(caso_resuelto),
+            mensaje="CASO RESUELTO:\n" + self.prettyprint_caso(caso_resuelto),
         )
-        self.debug_mensaje("DEBUG.retutilzar")
+        self.debug_mensaje("DEBUG.reutilizar")
 
-    def debug_revisar(self, caso_revisado, es_exito=None, es_corregido=None):
+    def debug_revisar(
+        self, caso_revisado: dict, es_exito: bool = None, es_corregido: bool = None
+    ) -> None:
+        """
+        Prints the information about the reviewed case.
+
+        Args:
+            - caso_revisado (dict): The reviewed case.
+            - es_exito (bool): The success flag.
+            - es_corregido (bool): The corrected flag.
+
+        Returns:
+            - None
+        """
         self.debug_mensaje(
             "DEBUG.revisar",
-            mensaje="CASO REVISADO: " + self.prettyprint_caso(caso_revisado),
+            mensaje="CASO REVISADO:\n" + self.prettyprint_caso(caso_revisado),
         )
         if es_exito is not None:
             self.debug_mensaje("DEBUG.revisar", mensaje="- exito: {}".format(es_exito))
@@ -104,10 +270,20 @@ class CBR_DEBUG:
             )
         self.debug_mensaje("DEBUG.revisar")
 
-    def debug_retener(self, caso_retenido, es_retenido=None):
+    def debug_retener(self, caso_retenido: dict, es_retenido: bool = None) -> None:
+        """
+        Prints the information about the retained case.
+
+        Args:
+            - caso_retenido (dict): The retained case.
+            - es_retenido (bool): The retention flag.
+
+        Returns:
+            - None
+        """
         self.debug_mensaje(
             "DEBUG.retener",
-            mensaje="CASO RETENIDO: " + self.prettyprint_caso(caso_retenido),
+            mensaje="CASO RETENIDO:\n" + self.prettyprint_caso(caso_retenido),
         )
         if es_retenido is not None:
             if es_retenido and ("id" in caso_retenido):
